5e97f03d83eecaf0b76668a71f8da9882545bcc4
Matt Rothberg <mrothber@cisco.com>
2019-03-15 10:20:38 -0400

    Now maps CDP & LLDP, as well as MAC to VLAN identifiers

--- API Stuff/test/2 - index - basics/test1.js
+++ API Stuff/test/2 - index - basics/test1.js
@@ -3,9 +3,11 @@ const MerakiDashboard = require('../../src/index');
 const expect = require('chai').expect; //???
 const MattsAPI = "611a8ceeedb0c36716a5125c46cd7f3ba760d465";
 const mattNetworkID = "L_647955396387940893";
+const mattSwitchSerial = "Q2HW-CV5L-5FXH";
 const chrisAPI = "314d5556e649ecfc77d1d08450daa571608be6d1";
 const chrisOrg = "169102";
 const chrisNetworkID = "N_600667600300592773";
+
 var currentNetworkID = chrisNetworkID; //set the current network here
 var currentAPI = chrisAPI; //set the current API key here
 let dashboard = MerakiDashboard(currentAPI);
@@ -20,27 +22,32 @@ dashboard.devices.list(currentNetworkID).then(function(result) { //this is parsi
   var arrayLength = result.length;
   for(var i = 0; i<arrayLength; i++){
     devices.push(result[i].serial);
+
     //console.log(result[i].serial) // "Some User token"
   }
+
+  //this is where we list the LLDP & CDP information of networking devices
+  console.log("CDP & LLDP INFORMATION: ");
   for(var x = 0; x < devices.length; x++){
     //console.log(devices[x]);
-    /*
+
     dashboard.clients.list(devices[x], "86400").then(function(clients) {
       var arrayLength1 = clients.length;
 
       for(var i = 0; i<arrayLength1; i++){ //this is taking the clients attached to each device
-        clients.push(clients[i].dhcpHostname);
-        console.log(clients[i].dhcpHostname);
+        clients.push(clients[i]);
+        console.log("MAC Address: " + clients[i].mac + ": VLAN: " + clients[i].vlan);
         //console.log(result[i].serial) // "Some User token"
       }
     })  .catch(function(e){
-      console.log("Error: " + e);
-    });*/
+      //console.log("Error: " + e);
+    });
     dashboard.devices.lldpCdpInfo(currentNetworkID, devices[x], "86400").then(function(lldpDevices){
       var arrayLength1 = lldpDevices.length;
-      console.log(lldpDevices.ports);
+      console.log("CDP Ports: " + lldpDevices.ports.wired0.cdp.portId);
+      console.log("LLDP Ports: " + lldpDevices.ports.wired0.lldp.portId)
     }) .catch(function(e){
-      console.log("Error: " + e);
+      //console.log("Error: " + e);
     });
 
   }
