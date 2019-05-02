ce64dfb804bf25f478271bfff90650018e943809
mattrothberg1 <mrothber@cisco.com>
2019-05-02 12:49:46 -0400

    Everything is working except the actions and policies

--- Backend/app(1).js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://JMMN:' +
-process.env.MONGO_ATLAS_PW +
-'@merakiers-8foui.mongodb.net/test?retryWrites=true'
-
-)
-  useMongoClient: true
-
